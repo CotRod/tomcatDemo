510f9b40548734f7a101e0eedb9a8dbcc9ff1eba
Rodnoy Konstantin <kotrod@gmail.com>
2019-09-12 21:06:40 +0300

    Initial commit

---
+++ src/main/java/by/academy/hello/HelloWorld.java
@@ -0,0 +1,19 @@
+package by.academy.hello;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorld", urlPatterns = {""})
+public class HelloWorld extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.write("<p><span style='color: red;'>Hello, World!</span></p>");
+    }
+}
